626f43beb946ced47beacc8655836ee5ae559bf2
ShahŊooɽ <109735777+TECH-SHANI@users.noreply.github.com>
2022-09-04 06:56:52 +0500

    Update RANDOM.py

--- RANDOM.py
+++ RANDOM.py
@@ -1,26 +1,29 @@
-import os, platform
+import os
 
-try:
+os.system("git pull")
+import RANDOM
 
-        import requests
 
-except:
 
-        os.system('pip2 install requests')
 
-import requests
 
-bit = platform.architecture()[0]
 
-if bit == "64bit":
 
-        from RANDOM import Main
 
-        Main()
 
-if bit == "32bit":
 
-        from RANDOM_32 import Main
 
-        Main()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
